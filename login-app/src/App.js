384d903eeb8b248b11d9c48663a15a80e3803d5d
Sneha Poulose K <sneha.poulose@rapidvaluesolutions.com>
2021-10-22 08:12:03 +0530

    -- created a login page.

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,19 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-import { render } from 'react-dom';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Login Page </h1>
-      <label>Username:
-      <input type="text" className="username" name="username"/>
-      </label>
-      <label>Password:
-      <input type="password" className="password" name="password"/>
-      </label>
-      <button type = "button" className = "Submit-button">Submit</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      username: "",
+      password: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    alert(' The entered username is : ' + this.state.username);
+    alert('The entered password is : ' + this.state.password);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="app">
+        <h1>Login Page </h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Username:
+            <input type="text"
+              className="username-input"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>Password:
+            <input type="password"
+              className="password-input"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange} />
+          </label>
+          <br />
+          <input type="submit" className="login-button" value="Login" />
+        </form>
+      </div>
+
+    )
+  }
 }
 export default App;
